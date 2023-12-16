b3c526730a6f59637d283610b7b4aafc31f41f60
wodeczw <czwmtnh@gmail.com>
2023-12-16 19:49:06 +0800

    一些测试记录的整理和.gitignore的编写

---
+++ some_record/猴子打野.py
@@ -0,0 +1,86 @@
+当前的线程iD==================== 21852
+当前任务: <Task pending name='Task-525' coro=<GameSessionManager.handle_room_session() running at C:\Users\Administrator\Desktop\code_project\python\become_comtributer\last_time\Parnrk\app\view\Parnrk\subscription.py:96> cb=[_wait.<locals>._on_completion() at H:\coder_tools\anaconda\envs\py38\Lib\asyncio\tasks.py:509]>
+8589679276
+当前的线程iD==================== 21852
+当前任务: <Task pending name='Task-550' coro=<GameSessionManager.handle_room_session() running at C:\Users\Administrator\Desktop\code_project\python\become_comtributer\last_time\Parnrk\app\view\Parnrk\subscription.py:96> cb=[_wait.<locals>._on_completion() at H:\coder_tools\anaconda\envs\py38\Lib\asyncio\tasks.py:509]>
+8589679276
+当前的线程iD==================== 21852
+当前任务: <Task pending name='Task-551' coro=<GameSessionManager.handle_room_session() running at C:\Users\Administrator\Desktop\code_project\python\become_comtributer\last_time\Parnrk\app\view\Parnrk\subscription.py:96> cb=[_wait.<locals>._on_completion() at H:\coder_tools\anaconda\envs\py38\Lib\asyncio\tasks.py:509]>
+8589679276
+当前的线程iD==================== 21852
+当前任务: <Task pending name='Task-594' coro=<GameSessionManager.handle_room_session() running at C:\Users\Administrator\Desktop\code_project\python\become_comtributer\last_time\Parnrk\app\view\Parnrk\subscription.py:96> cb=[_wait.<locals>._on_completion() at H:\coder_tools\anaconda\envs\py38\Lib\asyncio\tasks.py:509]>
+开始等待======================
+当前任务: <Task pending name='Task-595' coro=<GameSessionManager.handle_room_session() running at C:\Users\Administrator\Desktop\code_project\python\become_comtributer\last_time\Parnrk\app\view\Parnrk\subscription.py:96> cb=[_wait.<locals>._on_completion() at H:\coder_tools\anaconda\envs\py38\Lib\asyncio\tasks.py:509]>
+开始等待======================
+in here 
+len(allplayers) 10
+in here
+len(allplayers) 10
+game start 英雄名字: 齐天大圣
+add one
+game start 英雄名字: 离群之刺
+add one
+game start 英雄名字: 齐天大圣
+add one
+game start 英雄名字: 时间刺客
+add one
+game start 英雄名字: 离群之刺
+add one
+game start 英雄名字: 纳祖芒荣耀
+add one
+game start 英雄名字: 时间刺客
+add one
+game start 英雄名字: 暮光星灵
+add one
+game start 英雄名字: 纳祖芒荣耀
+add one
+game start 英雄名字: 皮城女警
+add one
+game start 英雄名字: 暮光星灵
+add one
+game start 英雄名字: 探险家
+add one
+game start 英雄名字: 皮城女警
+add one
+game start 英雄名字: 魂锁典狱长
+add one
+game start 英雄名字: 探险家
+add one
+game start 英雄名字: 封魔剑魂
+add one
+game start 英雄名字: 魂锁典狱长
+add one
+game start 英雄名字: 异画师
+add one
+len(puuid_champion_mapping)--- 10
+--------i: 0
+16dc57e9-70f3-5051-9abe-7eb7cad24bc0
+--------i: 1
+An error occurred: '42100863-3dae-5539-a77a-07e31928dacb'
+game start 英雄名字: 封魔剑魂
+add one
+game start 英雄名字: 异画师
+add one
+len(puuid_champion_mapping)--- 10
+--------i: 0
+16dc57e9-70f3-5051-9abe-7eb7cad24bc0
+--------i: 1
+An error occurred: '42100863-3dae-5539-a77a-07e31928dacb'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
